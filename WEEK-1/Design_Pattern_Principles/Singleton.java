92a13233d734aca58105e288fc2f47c81118afc1
Sravya192000 <vallabhanenisrisravya@gmail.com>
2026-06-19 22:07:55 +0530

    WEEK 1 DESIGN PATTERN PRINCIPLES

---
+++ WEEK-1/Design_Pattern_Principles/Singleton.java
@@ -0,0 +1,46 @@
+public class Singleton {
+
+    // Singleton Logger class
+    static class Logger {
+
+        // Single instance
+        private static Logger instance;
+
+        // Private constructor
+        private Logger() {
+            System.out.println("Logger instance created.");
+        }
+
+        // Method to get the single instance
+        public static Logger getInstance() {
+
+            if (instance == null) {
+                instance = new Logger();
+            }
+
+            return instance;
+        }
+
+        // Logging method
+        public void log(String message) {
+            System.out.println("LOG: " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application started");
+
+        logger2.log("User logged in");
+
+        if (logger1 == logger2) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances created.");
+        }
+    }
+}
