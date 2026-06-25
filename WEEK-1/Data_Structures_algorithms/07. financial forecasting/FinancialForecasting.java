78e5d1281aac32e66d44966686a070742b20e439
Sravya192000 <vallabhanenisrisravya@gmail.com>
2026-06-25 12:07:18 +0530

    week-1

---
+++ WEEK-1/Data_Structures_algorithms/07. financial forecasting/FinancialForecasting.java
@@ -0,0 +1,47 @@
+public class FinancialForecasting {
+
+    // Recursive method to calculate future value
+
+    public static double predictFutureValue(
+            double currentValue,
+            double growthRate,
+            int years) {
+
+        if (years == 0) {
+            return currentValue;
+        }
+
+        return predictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double futureValue = predictFutureValue(
+                currentValue,
+                growthRate,
+                years);
+
+        System.out.println(
+                "Current Value: Rs." +
+                        currentValue);
+
+        System.out.println(
+                "Growth Rate: " +
+                        (growthRate * 100) +
+                        "%");
+
+        System.out.println(
+                "Years: " + years);
+
+        System.out.println(
+                "Predicted Future Value: Rs." +
+                        futureValue);
+    }
+}
